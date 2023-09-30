07a148deb573965834ad639259441629200f0a89
lazidog <nvtien247@gmail.com>
2023-09-30 18:15:40 +0700

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
 import styles from './footer.module.css';
+
 export default function Footer() {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.ico" width={50} height={50} />
+          <h1 className={styles.logoText}>Lazilearning</h1>
+        </div>
+        <p className={styles.desc}>Developed by a lazy dog</p>
+      </div>
+    </div>
+  );
 }
